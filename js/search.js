9a0a3a9b9061757e44a81f5717d6e8a4b8b1ade8
jcompanion <joshcompanion@gmail.com>
2020-03-24 21:35:03 -0500

    Site Complete V1
    Initial Commit

---
+++ js/search.js
@@ -0,0 +1,11 @@
+
+
+$('#search').keyup(function () {
+    var value = $(this).val();
+    var exp = new RegExp(value, 'i');
+
+    $('a').each(function () {
+        var match = exp.test($(this).data('title'));
+        $(this).toggle(match);
+    });
+});
